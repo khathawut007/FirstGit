084664c269410a989c15a73fe8000c1d1cda2706
DESKTOP-Q3BUURO\khath <khathawut744@gmail.com>
2021-03-28 23:03:04 +0700

    สอบ

---
+++ สอบ.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+
+namespace สอบ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 3, b = 2, c = 4;
+            int player1, player2;
+
+            for (a = 3; a >= 3; a++)
+            {
+                for (b = 2; b >= 2; b++)
+                {
+                    for(c = 4; c >= 4; c++)
+                    {
+                        Console.Write("Tank: ");
+
+                        Console.WriteLine("Input number :");
+
+                        Console.WriteLine("Player  win");
+                    }
+                }
+            }
+                
+                
+                
+                
+                
+                
+                
+                
+                
+
+
+
+        }
+    }
+}
