a7eeecabe4b99501ad12f91b15f04a139fd61be3
DESKTOP-Q3BUURO\khath <khathawut744@gmail.com>
2021-03-28 21:23:14 +0700

    สอบ

---
+++ ทดสอบ1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ทดสอบ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int x = a;
+            int y = b;
+            if (x = y)
+            {
+                while (x < y)
+                {
+                    x = x + a;
+                }
+                y = y + b;
+            }
+            else
+            {
+                Console.WriteLine(x);
+            }
+            
+
+        }
+    }
+}
