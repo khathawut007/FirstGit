2eacae7680b7b19941d12ba65cedf86977c9ea59
DESKTOP-Q3BUURO\khath <khathawut744@gmail.com>
2021-04-24 14:43:54 +0700

    ข้อ6

---
+++ 6.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+
+            for (int row = 1; row <= b; row++)
+            {
+                for (int column = 1; column <= b; column++)
+                {
+                    Console.Write(a);
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Horizontal(int x,int z)
+        {
+            for(int row =1; row <= x; row++)
+            {
+                for (int column = 1; column <= z; column++)
+                {
+                    
+                }
+                Console.WriteLine();
+            }
+        }
+       
+        
+    }
+}
