2620ea412ddf1d2fbfc4950ff8590024e9e064fb
khathawut007 <78632160+khathawut007@users.noreply.github.com>
2021-09-18 14:50:19 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,34 +1,12 @@
 ﻿using System;
-
-namespace ใหม่
+using System.Collections.Generic;
+namespace Quiz
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string mode = Console.ReadLine();
-            double x = double.Parse(Console.ReadLine());
-            double y = double.Parse(Console.ReadLine());
-
-            switch (mode)
-            {
-                case "time":
-                    Console.WriteLine("{0}  {1}",x, Math.Pow(y - 1, 2));
-                    break;
-                case "price":
-                    Console.WriteLine("{0}  {1}",Math.Pow(x - 1, 2),y);
-                    break;
-                default:
-                    Console.WriteLine("Invalid mode");
-                    break;
-
-            }
-            if (x < 0)
-            Console.WriteLine("Invalid mouse position");
-            
-            Console.ReadLine();
-
-
+            InforFlw Rose = Console.WriteLine("infomation:{0}",Rose);
         }
     }
 }
